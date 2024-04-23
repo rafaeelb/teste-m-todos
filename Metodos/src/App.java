bc72116d1223cdb14b0c47d7c044225ada5cff5a
Rafael <rafael2004benicio@gmail.com>
2024-04-23 18:59:09 -0300

    [feat] commit utilizando joptionpane

--- Metodos/src/App.java
+++ Metodos/src/App.java
@@ -12,11 +12,11 @@ public class App {
         // instrução do while que solicita a senha do celular e logo após entra na condição de que se a senha for = 1908, retorna true e com a mensagem Senha correta, se não for igual a 1908 retorna false com a mensagem Senha incorreta e fica no loop até que a senha seja igual a 1908.
         do {
             senha = Integer.parseInt(JOptionPane.showInputDialog(null,"Qual a senha do meu celular?"));
-        if (cel1.inserirSenha(senha)) {
-            JOptionPane.showMessageDialog(null,"A senha está correta");
-        }else{
-            JOptionPane.showMessageDialog(null, "A senha está incorreta, digite novamente");
-        }
+            if (cel1.inserirSenha(senha)) {
+                JOptionPane.showMessageDialog(null,"A senha está correta");
+            }else{
+                JOptionPane.showMessageDialog(null, "A senha está incorreta, digite novamente");
+            }
         } while (cel1.inserirSenha(senha) == false);
         
 
