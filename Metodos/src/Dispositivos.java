272ddaad7c0a8739a24767c9cb609b5767af526b
Rafael <rafael2004benicio@gmail.com>
2024-05-04 13:34:57 -0300

    [refactor] definindo atributos como protected na classe dispositivos

--- Metodos/src/Dispositivos.java
+++ Metodos/src/Dispositivos.java
@@ -1,6 +1,6 @@
 public abstract class Dispositivos {
-    public int senha;
-    public int botao_volume;
+    protected int senha;
+    protected int botao_volume;
     
     public abstract boolean inserirSenha(int senha);
     public abstract boolean aumentarVolume(int botao_volume);
